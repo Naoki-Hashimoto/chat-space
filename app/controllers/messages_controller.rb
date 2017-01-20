c8415e6634e5b0cafc717bfd8adae941bf0fd2fe
Naoki-Hashimoto <naokihashimoto@naokinoMacBook-Pro.local>
2017-01-20 17:42:54 +0900

    Change the contents about before_action.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,4 @@
 class MessagesController < ApplicationController
-  before_action :move_to_sign_in
-
   def index
-  end
-
-  def move_to_sign_in
-    redirect_to "/users/sign_in" unless user_signed_in?
   end
 end
