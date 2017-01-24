610ff57a2d36024bf552d224f0484ba84b18422d
Naoki-Hashimoto <naokihashimoto@naokinoMacBook-Pro.local>
2017-01-24 17:20:08 +0900

    Preserve informations was submitted

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,4 +1,23 @@
 class GroupController < ApplicationController
+
   def new
+    @group = Group.new
   end
+
+  def create
+    @group = Group.new(post_params)
+    if @group.save
+      redirect_to root_path(@group),notice: 'チャットグループが作成されました。'
+    else
+      flash[:alert] = '保存に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:group).permit(:name, user_ids:[] )
+  end
+
+
 end
