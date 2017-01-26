310bc0a93a6662a43eafdeab8e50cd774039df54
Naoki-Hashimoto <naokihashimoto@naokinoMacBook-Pro.local>
2017-01-26 19:44:39 +0900

    Revice views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :groups, only: [:new, :edit, :create, :update]
+  root 'groups#index'
+  resources :groups, only: [:index,:new, :edit, :create, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
