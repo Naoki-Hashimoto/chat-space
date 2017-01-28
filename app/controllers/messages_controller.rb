d8e2ff7ca7ed1bd34dd48d10144c5c452a667b6d
Naoki-Hashimoto <naokihashimoto@naokinoMacBook-Pro.local>
2017-01-28 13:53:35 +0900

    Setting CreateAction.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,26 @@ class MessagesController < ApplicationController
   before_action :get_group_id
   def index
     @groups = current_user.groups
+    @message = current_user.messages.new
+  end
+
+  def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: 'メッセージの送信が完了しました。'
+    else
+      flash.now[:alert] = 'メッセージの送信に失敗しました。'
+      render :index
+    end
   end
 
   private
   def get_group_id
     @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
 
 end
