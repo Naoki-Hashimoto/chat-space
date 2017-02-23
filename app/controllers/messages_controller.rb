faf3ba36a6f0c7185d3dad70b2d1c14b28f0fbeb
Naoki-Hashimoto <naokihashimoto@naokinoMacBook-Pro.local>
2017-02-23 17:07:16 +0900

    Add writing to reload.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,11 @@ class MessagesController < ApplicationController
   before_action :set_group
   def index
     @message = Message.new
+    @messages = @group.messages.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
